90670ee64f9fa2c3b499e78284247c51f448e16a
OsimFavour <famos204@gmail.com>
2023-12-28 00:32:43 +0100

    Change Footer write-up

--- src/components/Footer/BottomFooter.jsx
+++ src/components/Footer/BottomFooter.jsx
@@ -7,7 +7,7 @@ const BottomFooter = () => {
                 <div className="row">
                     <div className="col-12">
                         <p className="text-center mb-0 text-white">
-                            &copy; {new Date().getFullYear()}; Powered by amazon
+                            &copy; {new Date().getFullYear()}. Powered by amazon
                         </p>
                     </div>
                 </div>
