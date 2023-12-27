b5c6c7164ad3adbad69958bdf28bbdb3b84ce010
OsimFavour <famos204@gmail.com>
2023-12-27 10:51:07 +0100

    Change Banner style

--- src/pages/Home/SmallBanner.jsx
+++ src/pages/Home/SmallBanner.jsx
@@ -2,21 +2,8 @@ import React from 'react'
 
 const SmallBanner = () => {
     return (
-        <div className="d-flex flex-wrap justify-content-between align-items-center">
-            <div className="small-banner-content position-relative p-3">
-                <img
-                    src="images/catbanner-01.jpg" 
-                    className='img-fluid rounded-3'
-                    alt="main banner" 
-                />
-                <div className="small-banner-content position-absolute">
-                    <h4>Supercharged For Pros</h4>
-                    <h5>iPad S13+ Pro.</h5>
-                    <p>From $999.00 or $41.62/mo</p>
-                </div>
-            </div>
-
-            <div className="small-banner-content position-relative p-3">
+        <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+            <div className="small-banner position-relative">
                 <img
                     src="images/catbanner-01.jpg" 
                     className='img-fluid rounded-3'
@@ -24,34 +11,47 @@ const SmallBanner = () => {
                 />
                 <div className="small-banner-content position-absolute">
                     <h4>Best Sale</h4>
-                    <h5>iPad S13+ Pro.</h5>
-                    <p>From $999.00 or $41.62/mo</p>
+                    <h5>Laptops Max</h5>
+                    <p>From $1699.00 <br/> or $64.62/mo</p>
                 </div>
-            </div>
+            </div> 
 
-            <div className="small-banner-content position-relative p-3">
+            <div className="small-banner position-relative">
                 <img
-                    src="images/catbanner-02.jpg" 
+                    src="images/catbanner-03.jpg" 
                     className='img-fluid rounded-3'
                     alt="main banner" 
                 />
                 <div className="small-banner-content position-absolute">
                     <h4>New Arrival</h4>
                     <h5>Buy iPad Air</h5>
-                    <p>From $999.00 or $41.62/mo</p>
+                    <p>From $599.00 or <br/>$49.91/mo for 12mo.*</p>
                 </div>
             </div>
 
-            <div className="small-banner-content position-relative p-3">
+            <div className="small-banner position-relative">
                 <img
-                    src="images/catbanner-01.jpg" 
+                    src="images/catbanner-02.jpg" 
                     className='img-fluid rounded-3'
                     alt="main banner" 
                 />
                 <div className="small-banner-content position-absolute">
-                    <h4>Supercharged For Pros</h4>
-                    <h5>iPad S13+ Pro.</h5>
-                    <p>From $999.00 or $41.62/mo</p>
+                    <h4>16% off</h4>
+                    <h5>SmartWatch 7.</h5>
+                    <p>Shop the latest band <br/>styles and colors.</p>
+                </div>
+            </div>
+
+            <div className="small-banner position-relative">
+                <img
+                    src="images/catbanner-04.jpg" 
+                    className='img-fluid rounded-3'
+                    alt="main banner" 
+                />
+                <div className="small-banner-content position-absolute">
+                    <h4>Free Engraving</h4>
+                    <h5>AirPods Max</h5>
+                    <p>High-fidelity playback & <br/>ultra-low distortion</p>
                 </div>
             </div>
             
