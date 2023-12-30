d6e709ddd0161fee01dff48a22db72779b8b9e8b
OsimFavour <famos204@gmail.com>
2023-12-30 15:55:41 +0100

    Add Links

--- src/components/Header/Actions.jsx
+++ src/components/Header/Actions.jsx
@@ -5,7 +5,7 @@ const Actions = () => {
     return (
         <div className="header-upper-links d-flex align-items-center justify-content-between pb-4">
             <div>
-                <Link className='d-flex align-items-center gap-10 text-white'>
+                <Link to='/compare-product' className='d-flex align-items-center gap-10 text-white'>
                     <img src="/images/compare.svg" alt="compare" />
                     <p className='mb-0 p-font'>
                         Compare <br/> Products
@@ -14,7 +14,7 @@ const Actions = () => {
             </div>
 
             <div>
-                <Link className='d-flex align-items-center gap-10 text-white'>
+                <Link to='/wishlist' className='d-flex align-items-center gap-10 text-white'>
                     <img src="/images/wishlist.svg" alt="wishlist" />
                     <p className='mb-0 p-font'>
                         Favourite <br/> Products
@@ -23,7 +23,7 @@ const Actions = () => {
             </div>
 
             <div>
-                <Link className='d-flex align-items-center gap-10 text-white'>
+                <Link to='/login' className='d-flex align-items-center gap-10 text-white'>
                     <img src="/images/user.svg" alt="user" />
                     <p className='mb-0 p-font'>
                         Log in <br/> My Account
@@ -32,7 +32,7 @@ const Actions = () => {
             </div>
 
             <div>
-                <Link className='d-flex align-items-center gap-10 text-white'>
+                <Link to='/cart' className='d-flex align-items-center gap-10 text-white'>
                     <img src="/images/cart.svg" alt="cart" />
                     <div className="d-flex flex-column gap-10 mb-0">
                         <span className="badge bg-white text-dark">0</span>
