568924cd3527b7f2eae3f36a20de114599058f44
OsimFavour <famos204@gmail.com>
2023-12-30 15:58:45 +0100

    Refactor Code

--- src/components/Footer/MiddleFooter.jsx
+++ src/components/Footer/MiddleFooter.jsx
@@ -10,17 +10,17 @@ const MiddleFooter = () => {
                 <div className="row">
 
                     <div className="col-4">
-                        <h4 className='text-white mb-4'>Contact Us</h4>
+                        <h4 className='footer-header'>Contact Us</h4>
                         <div>
-                            <address className='text-white fs-6'>
+                            <address className='text-white fs-6 m-font'>
                                 15 Afukang Lane, <br /> Sunny Vale <br /> Postal Code: 580311
                             </address>
 
-                            <a href="tel:+233 678 493 4394" className='mt-3 d-block mb-1 text-white'>
+                            <a href="tel:+233 678 493 4394" className='mt-3 d-block mb-1 text-white m-font'>
                             +233 678 493 4394
                             </a>
 
-                            <a href="mailto:balablue@gmail.com" className='mt-2 d-block mb-0 text-white mb-4'>balared@gmail.com</a>
+                            <a href="mailto:balablue@gmail.com" className='mt-2 d-block mb-0 text-white mb-4 m-font'>balared@gmail.com</a>
                             
                             <div className="social-icons d-flex align-items-center gap-30">
                                 <a className='text-white' href="#">
@@ -41,37 +41,37 @@ const MiddleFooter = () => {
 
 
                     <div className="col-3">
-                        <h4 className='text-white mb-4'>Information</h4>
-                        <div className='footer-link d-flex flex-column'>
-                            <Link className='text-white py-2 mb-1'>Privacy Policy</Link>
-                            <Link className='text-white py-2 mb-1'>Refund Policy</Link>
-                            <Link className='text-white py-2 mb-1'>Shipping Policy</Link>
-                            <Link className='text-white py-2 mb-1'>Terms & Conditions</Link>
-                            <Link className='text-white py-2 mb-1'>Blogs</Link>
+                        <h4 className='footer-header'>Information</h4>
+                        <div className='d-flex flex-column'>
+                            <Link className='footer-link'>Privacy Policy</Link>
+                            <Link className='footer-link'>Refund Policy</Link>
+                            <Link className='footer-link'>Shipping Policy</Link>
+                            <Link className='footer-link'>Terms & Conditions</Link>
+                            <Link className='footer-link'>Blogs</Link>
                         </div>
                     </div>
 
 
                     <div className="col-3">
-                        <h4 className='text-white mb-4'>Account</h4>
-                        <div className='footer-link d-flex flex-column'>
-                        <Link className='text-white py-2 mb-1'>Search</Link>
-                            <Link className='text-white py-2 mb-1'>About Us</Link>
-                            <Link className='text-white py-2 mb-1'>Faq</Link>
-                            <Link className='text-white py-2 mb-1'>Contact</Link>
-                            <Link className='text-white py-2 mb-1'>Size Chart</Link>
+                        <h4 className='footer-header'>Account</h4>
+                        <div className='d-flex flex-column'>
+                            <Link className='footer-link'>Search</Link>
+                            <Link className='footer-link'>About Us</Link>
+                            <Link className='footer-link'>Faq</Link>
+                            <Link className='footer-link'>Contact</Link>
+                            <Link className='footer-link'>Size Chart</Link>
                         </div>
                     </div>
 
 
                     <div className="col-2">
-                        <h4 className='text-white mb-4'>Quick Links</h4>
-                        <div className='footer-link d-flex flex-column'>
-                            <Link className='text-white py-2 mb-1'>Accessories</Link>
-                            <Link className='text-white py-2 mb-1'>Laptops</Link>
-                            <Link className='text-white py-2 mb-1'>Headphones</Link>
-                            <Link className='text-white py-2 mb-1'>Smart Watches</Link>
-                            <Link className='text-white py-2 mb-1'>Tablets</Link>
+                        <h4 className='footer-header'>Quick Links</h4>
+                        <div className='d-flex flex-column'>
+                            <Link className='footer-link'>Accessories</Link>
+                            <Link className='footer-link'>Laptops</Link>
+                            <Link className='footer-link'>Headphones</Link>
+                            <Link className='footer-link'>Smart Watches</Link>
+                            <Link className='footer-link'>Tablets</Link>
                         </div>
                     </div>
                 </div>
