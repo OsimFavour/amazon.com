6f05a926f1c9bf9f77b57284bf63c63551756d23
OsimFavour <famos204@gmail.com>
2023-12-30 15:50:27 +0100

    Add props to product card component

--- src/pages/Store/StoreProductsList.jsx
+++ src/pages/Store/StoreProductsList.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ProductCard from '../../components/ProductCard'
 
-const StoreProductsList = () => {
+const StoreProductsList = ({ grid }) => {
     return (
         <div className="products-list pb-5">
-            <ProductCard/>
+            <div className="d-flex gap-10 flex-wrap">
+                <ProductCard grid={grid}/>
+                <ProductCard grid={grid}/>
+            </div>
         </div>
     )
 }
