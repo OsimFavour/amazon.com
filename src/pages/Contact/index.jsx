583a3061e37dab32c63c77ee745d40bc85ed458a
OsimFavour <famos204@gmail.com>
2023-12-30 15:53:45 +0100

    Refactor code with new MapLocation and ContactForm Component

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Meta from '../../components/Meta'
+import BreadCrumb from '../../components/BreadCrumb'
+import MapLocation from './MapLocation'
+import ContactForm from './ContactForm'
+
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <>
+
+            <Meta title='Contact Us'/>
+            <BreadCrumb title='Contact Us'/>
+
+            <div className="contact-wrapper py-5 home-wrapper-2">
+                <div className="container">
+                    <div className="row">
+                        
+                        <MapLocation/>
+
+                        <ContactForm/>
+                    </div>
+                </div>
+            </div>
+
+        </>
     )
 }
 
