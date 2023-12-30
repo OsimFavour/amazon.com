9cf0c8602fabfdf110f14150ce900c34ec3f82f4
OsimFavour <famos204@gmail.com>
2023-12-30 15:48:38 +0100

    Create Blog Page

---
+++ src/pages/Blog/BlogCategories.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const BlogCategories = () => {
+    return (
+        <div className='filter-card mb-3'>
+            <h3 className="filter-title">Blog Categories</h3>
+
+            <div>
+                <ul className="ps-0">
+                    <li>Home</li>
+                    <li>Our Store</li>
+                    <li>Blogs</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default BlogCategories
---
+++ src/pages/Blog/index.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Meta from '../../components/Meta'
+import BreadCrumb from '../../components/BreadCrumb'
+import BlogCard from '../../components/BlogCard'
+import BlogCategories from './BlogCategories'
+
+
+const Blog = () => {
+    return (
+        <>
+            <Meta title='Me Blog'/>
+            <BreadCrumb title='Blogs'/>
+
+            <div className="blog-wrapper home-wrapper-2 py-5">
+                <div className="container">
+                    <div className="row">
+
+                        <div className="col-3">
+                            
+                            <BlogCategories/>
+
+                        </div>
+
+
+                        <div className="col-9">
+                            <div className="row">
+                                
+                                <div className="col-6 mb-3">
+                                    <BlogCard/>
+                                </div>
+
+                                <div className="col-6 mb-3">
+                                    <BlogCard/>
+                                </div>
+
+                                <div className="col-6 mb-3">
+                                    <BlogCard/>
+                                </div>
+
+                                <div className="col-6 mb-3">
+                                    <BlogCard/>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Blog
