9e1206b822b31296120a28d7179ecd9a0b7dcc22
OsimFavour <famos204@gmail.com>
2023-12-30 16:02:48 +0100

    Add New blog, and store route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import React from 'react'
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import About from './pages/About/About';
-import Contact from './pages/Contact';
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import About from './pages/About/About'
+import Contact from './pages/Contact'
+import Store from './pages/Store'
+import Blog from './pages/Blog'
+import CompareProduct from './pages/CompareProduct'
 
 
 function App() {
@@ -16,6 +19,9 @@ function App() {
           <Route index element={<Home/>}/>
           <Route path='about' element={<About/>}/>
           <Route path='contact' element={<Contact/>}/>
+          <Route path='store' element={<Store/>}/>
+          <Route path='blog' element={<Blog/>}/>
+          <Route path='compare-product' element={<CompareProduct/>}/>
         </Route>
       </Routes>
      </BrowserRouter> 
@@ -23,4 +29,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
