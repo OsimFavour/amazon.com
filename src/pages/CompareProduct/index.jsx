c7015cc02a1582a0c30885fc0a55000f804aa8a7
OsimFavour <famos204@gmail.com>
2024-01-02 16:18:11 +0100

    Refactor Code

--- src/pages/CompareProduct/index.jsx
+++ src/pages/CompareProduct/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Meta from '../../components/Meta'
 import BreadCrumb from '../../components/BreadCrumb'
+import Color from '../../components/Color'
+import CompareProductCard from './CompareProductCard'
 
 
 const CompareProduct = () => {
@@ -8,6 +10,20 @@ const CompareProduct = () => {
     <>
       <Meta title='Compare Products'/>
       <BreadCrumb title='Compare Products'/>
+
+      <div className="compare-product-wrapper py-5 home-wrapper-2">
+        <div className="container">
+          <div className="row">
+
+            <CompareProductCard/>
+
+            <CompareProductCard/>
+
+            
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
