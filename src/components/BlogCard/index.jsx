577894efe5c4223ddce7f62bb6076f158be19491
OsimFavour <famos204@gmail.com>
2024-01-05 12:45:00 +0100

    Add New Blog Image

--- src/components/BlogCard/index.jsx
+++ src/components/BlogCard/index.jsx
@@ -6,7 +6,7 @@ const BlogCard = () => {
        <>
             <div className="blog-card">
                 <div className="card-image">
-                    <img src="images/blog-1.jpg" className='img-fluid w-100' alt="blog" />
+                    <img src="images/blog.jpg" className='img-fluid w-100' alt="blog" />
                 </div>
 
                 <div className="blog-content">
