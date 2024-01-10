112fe9aa8703ac093c70f3d034694ed6aeb36e67
OsimFavour <famos204@gmail.com>
2024-01-10 12:20:14 +0100

    Refactor to add Container Component

--- src/pages/Home/FeaturedSection.jsx
+++ src/pages/Home/FeaturedSection.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import ProductCard from '../../components/ProductCard'
+import Container from '../../components/Container'
 
 const FeaturedSection = () => {
     return (
-        <section className="featured-wrapper py-5 home-wrapper-2">
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <h3 className="section-heading">
-                            Our Featured Collection
-                        </h3>
-                    </div>
-                    
-                    <ProductCard/>
-                    <ProductCard/>
-                    <ProductCard/>
-                    <ProductCard/>
-                    
+       
+
+        <Container class1='featured-wrapper py-5 home-wrapper-2'>
+
+            <div className="row">
+                <div className="col-12">
+                    <h3 className="section-heading">
+                        Our Featured Collection
+                    </h3>
                 </div>
+                
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+                    
             </div>
-        </section>
+
+        </Container>
     )
 }
 
