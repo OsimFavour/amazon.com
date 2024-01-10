bfa2a8cdfa0f16295b6a52d0cfda1d0e04f8ab2b
OsimFavour <famos204@gmail.com>
2024-01-10 12:16:28 +0100

    Change class to className

--- src/pages/ProductView/MainProductImage.jsx
+++ src/pages/ProductView/MainProductImage.jsx
@@ -4,42 +4,42 @@ const MainProductImage = () => {
   return (
     
     <div className='col-6'>
-        <div class="row py-3 shadow">
-            <div class="col-12 mb-1">
-                <div class="lightbox">
+        <div className="row py-3 shadow">
+            <div className="col-12 mb-1">
+                <div className="lightbox">
                     <img
                         src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/14a.webp"
                         alt="Gallery image 1"
-                        class="ecommerce-gallery-main-img active w-100"
+                        className="ecommerce-gallery-main-img active w-100"
                     />
                 </div>
             </div>
-            <div class="col-3 mt-1">
+            <div className="col-3 mt-1">
                 <img
                     src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/14a.webp"
                     alt="Gallery image 1"
-                    class="active w-100"
+                    className="active w-100"
                 />
             </div>
-            <div class="col-3 mt-1">
+            <div className="col-3 mt-1">
                 <img
                     src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/12a.webp"
                     alt="Gallery image 2"
-                    class="w-100"
+                    className="w-100"
                 />
             </div>
-            <div class="col-3 mt-1">
+            <div className="col-3 mt-1">
                 <img
                     src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/13a.webp"
                     alt="Gallery image 3"
-                    class="w-100"
+                    className="w-100"
                 />
             </div>
-            <div class="col-3 mt-1">
+            <div className="col-3 mt-1">
                 <img
                     src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/15a.webp"
                     alt="Gallery image 4"
-                    class="w-100"
+                    className="w-100"
                 />
             </div>
         </div>
