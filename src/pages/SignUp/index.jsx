a3a66d72ffbccd24c414a0445aac2d3203c95e32
OsimFavour <famos204@gmail.com>
2024-01-10 12:11:35 +0100

    Refactor to add Container and CustomInput Component

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Meta from '../../components/Meta'
 import BreadCrumb from '../../components/BreadCrumb'
+import Container from '../../components/Container'
+import CustomInput from '../../components/CustomInput'
 
 
 const SignUp = () => {
@@ -10,66 +12,34 @@ const SignUp = () => {
             <BreadCrumb title='Sign Up'/>
 
 
-            <div className='auth-wrapper py-5 home-wrapper-2'>
-                <div className="container">
-
-                    <div className='row'>
-                        <div className='col-12'>
-
-                            <div className='auth-card'>
-                                <h3>Sign Up</h3>
-                                <form action='' className='auth-form'>
-                                    <div>
-                                        <input 
-                                            type='text' 
-                                            name='firstName'
-                                            placeholder='First Name'
-                                            className='form-control'
-                                        />
-                                    </div>
-
-                                    <div>
-                                        <input 
-                                            type='text' 
-                                            name='lastName'
-                                            placeholder='Last Name'
-                                            className='form-control'
-                                        />
-                                    </div>
-
-                                    <div>
-                                        <input 
-                                            type='email' 
-                                            name='email'
-                                            placeholder='Email'
-                                            className='form-control'
-                                        />
-                                    </div>
-
-                                    <div className='mt-1 mb-3'>
-                                        <input 
-                                            type='password' 
-                                            name='password'
-                                            placeholder='Password'
-                                            className='form-control' 
-                                        />
-                                    </div>
-
-                                    <div>
-
-                                        <div className='auth-button'>
-                                            <button className='button border-0' type='submit'>Sign Up</button>
-                                        </div>
-                                    </div>
-                                    
-                                </form>
-                            </div>
-
-                        </div>
-                    </div>
+            <Container class1='auth-wrapper py-5 home-wrapper-2'>
                 
+                <div className='row'>
+                    <div className='col-12'>
+
+                        <div className='auth-card'>
+                            <h3>Sign Up</h3>
+                            <form action='' className='auth-form'>
+                                
+                                <CustomInput type='text' name='firstName' placeholder='First Name' />
+                                <CustomInput type='text' name='lastName' placeholder='Last Name' />
+                                <CustomInput type='email' name='email' placeholder='Email' />
+                                <CustomInput type='password' name='password' placeholder='Password' className='mt-1 mb-3' />
+
+                                <div>
+
+                                    <div className='auth-button'>
+                                        <button className='button border-0' type='submit'>Sign Up</button>
+                                    </div>
+                                </div>
+                                
+                            </form>
+                        </div>
+
+                    </div>
                 </div>
-            </div>
+                
+            </Container>
         </>
     )
 }
