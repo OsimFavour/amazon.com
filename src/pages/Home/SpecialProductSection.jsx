5e70bb8ce12df77098cfb9a77a56fefaba211161
OsimFavour <famos204@gmail.com>
2024-01-10 12:17:46 +0100

    Refactor to add Container Component

--- src/pages/Home/SpecialProductSection.jsx
+++ src/pages/Home/SpecialProductSection.jsx
@@ -1,25 +1,24 @@
 import React from 'react'
 import SpecialProductCard from '../../components/SpecialProductCard'
+import Container from '../../components/Container'
 
 const SpecialProductSection = () => {
     return (
-        <section className="special-wrapper py-5 home-wrapper-2">
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <h3 className="section-heading">
-                           Special Products
-                        </h3>
-                    </div>
-                </div>
-
-                <div className="row">
-                    <SpecialProductCard/>
-                    <SpecialProductCard/>
-                    <SpecialProductCard/>
+        <Container class1="special-wrapper py-5 home-wrapper-2">
+            <div className="row">
+                <div className="col-12">
+                    <h3 className="section-heading">
+                        Special Products
+                    </h3>
                 </div>
             </div>
-        </section>
+
+            <div className="row">
+                <SpecialProductCard/>
+                <SpecialProductCard/>
+                <SpecialProductCard/>
+            </div>
+        </Container>
     )
 }
 
