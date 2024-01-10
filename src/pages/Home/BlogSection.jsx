4e66fab5734d05dacd198e05f7f0e62cfee6ec9d
OsimFavour <famos204@gmail.com>
2024-01-10 12:20:56 +0100

    Refactor to add Container Component

--- src/pages/Home/BlogSection.jsx
+++ src/pages/Home/BlogSection.jsx
@@ -1,36 +1,35 @@
 import React from 'react'
 import BlogCard from '../../components/BlogCard'
+import Container from '../../components/Container'
 
 const BlogSection = () => {
     return (
-        <section className="blog-wrapper py-5 home-wrapper-2">
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <div className="section-heading">
-                            Our Latest News
-                        </div>
+        <Container class1="blog-wrapper py-5 home-wrapper-2">
+            <div className="row">
+                <div className="col-12">
+                    <div className="section-heading">
+                        Our Latest News
                     </div>
-
-                    <div className="col-md-4 mb-5">
-                        <BlogCard/>
-                    </div>
-
-                    <div className="col-md-4 mb-5">
-                        <BlogCard/>
-                    </div>
-
-                    <div className="col-md-4 mb-5">
-                        <BlogCard/>
-                    </div>
-
-                    <div className="col-md-4 mb-5">
-                        <BlogCard/>
-                    </div>
-                    
                 </div>
+
+                <div className="col-md-4 mb-5">
+                    <BlogCard/>
+                </div>
+
+                <div className="col-md-4 mb-5">
+                    <BlogCard/>
+                </div>
+
+                <div className="col-md-4 mb-5">
+                    <BlogCard/>
+                </div>
+
+                <div className="col-md-4 mb-5">
+                    <BlogCard/>
+                </div>
+                
             </div>
-        </section>
+        </Container>
     )
 }
 
