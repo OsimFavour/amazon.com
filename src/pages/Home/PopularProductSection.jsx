8bb29d762bc02d04fbd63c233564609bbba42f7f
OsimFavour <famos204@gmail.com>
2024-01-10 12:19:01 +0100

    Refactor to add Container Component

--- src/pages/Home/PopularProductSection.jsx
+++ src/pages/Home/PopularProductSection.jsx
@@ -1,25 +1,24 @@
 import React from 'react'
 import ProductCard from '../../components/ProductCard'
+import Container from '../../components/Container'
 
 const PopularProductSection = () => {
   return (
-    <section className="popular-wrapper py-5 home-wrapper-2">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <h3 className="section-heading">
-                        Our Popular Products
-                    </h3>
-                </div>
-                
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                
+    <Container class1="popular-wrapper py-5 home-wrapper-2">
+        <div className="row">
+            <div className="col-12">
+                <h3 className="section-heading">
+                    Our Popular Products
+                </h3>
             </div>
+            
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            
         </div>
-    </section>
+    </Container>
   )
 }
 
