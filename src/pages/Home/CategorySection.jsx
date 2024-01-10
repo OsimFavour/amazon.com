3d29358f6b393dcd3f3ba221aaaef9d8d7f270bc
OsimFavour <famos204@gmail.com>
2024-01-10 12:20:41 +0100

    Refactor to add Container Component

--- src/pages/Home/CategorySection.jsx
+++ src/pages/Home/CategorySection.jsx
@@ -1,84 +1,86 @@
 import React from 'react'
+import Container from '../../components/Container'
 
 const CategorySection = () => {
     return (
-        <section className="home-wrapper-2 py-5">
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <div className="categories d-flex justify-content-between flex-wrap align-items-center">
-                            <div className='d-flex gap-3 align-items-center'>
-                                <div>
-                                    <h6 className='md-font'>Cameras</h6>
-                                    <p className='p-font'>10 Items</p>
-                                </div>
-                                <img src="images/camera.jpg" alt="camera" />
+
+        <Container class1='home-wrapper-2 py-5'>
+
+            <div className="row">
+                <div className="col-12">
+                    <div className="categories d-flex justify-content-between flex-wrap align-items-center">
+                        <div className='d-flex gap-3 align-items-center'>
+                            <div>
+                                <h6 className='md-font'>Cameras</h6>
+                                <p className='p-font'>10 Items</p>
                             </div>
-
-                            <div className='d-flex gap-3 align-items-center'>
-                                <div>
-                                    <h6 className='md-font'>Smart TV</h6>
-                                    <p className='p-font'>10 Items</p>
-                                </div>
-                                <img src="images/tv.jpg" alt="television" />
-                            </div>
-
-                            <div className='d-flex gap-3 align-items-center'>
-                                <div>
-                                    <h6 className='md-font'>Music</h6>
-                                    <p className='p-font'>10 Items</p>
-                                </div>
-                                <img src="images/headphone.jpg" alt="wrist watches" />
-                            </div>
-
-                            <div className='d-flex gap-3 align-items-center'>
-                                <div>
-                                    <h6 className='md-font'>Gaming</h6>
-                                    <p className='p-font'>10 Items</p>
-                                </div>
-                                <img src="images/tv.jpg" alt="music and games" />
-                            </div>
-
-
-                            
-
-                            <div className='d-flex gap-3 align-items-center'>
-                                <div>
-                                    <h6 className='md-font'>Laptops</h6>
-                                    <p className='p-font'>10 Items</p>
-                                </div>
-                                <img src="images/laptop.jpg" alt="television" />
-                            </div>
-
-                            <div className='d-flex gap-3 align-items-center'>
-                                <div>
-                                    <h6 className='md-font'>Tablets</h6>
-                                    <p className='p-font'>10 Items</p>
-                                </div>
-                                <img src="images/camera.jpg" alt="camera" />
-                            </div>
-
-                            <div className='d-flex gap-3 align-items-center'>
-                                <div>
-                                    <h6 className='md-font'>Speakers</h6>
-                                    <p className='p-font'>10 Items</p>
-                                </div>
-                                <img src="images/speaker.jpg" alt="wrist watches" />
-                            </div>
-
-                            <div className='d-flex gap-3 align-items-center'>
-                                <div>
-                                    <h6 className='md-font'>Appliances</h6>
-                                    <p className='p-font'>10 Items</p>
-                                </div>
-                                <img src="images/homeapp.jpg" alt="music and games" />
-                            </div> 
-                            
+                            <img src="images/camera.jpg" alt="camera" />
                         </div>
+
+                        <div className='d-flex gap-3 align-items-center'>
+                            <div>
+                                <h6 className='md-font'>Smart TV</h6>
+                                <p className='p-font'>10 Items</p>
+                            </div>
+                            <img src="images/tv.jpg" alt="television" />
+                        </div>
+
+                        <div className='d-flex gap-3 align-items-center'>
+                            <div>
+                                <h6 className='md-font'>Music</h6>
+                                <p className='p-font'>10 Items</p>
+                            </div>
+                            <img src="images/headphone.jpg" alt="wrist watches" />
+                        </div>
+
+                        <div className='d-flex gap-3 align-items-center'>
+                            <div>
+                                <h6 className='md-font'>Gaming</h6>
+                                <p className='p-font'>10 Items</p>
+                            </div>
+                            <img src="images/tv.jpg" alt="music and games" />
+                        </div>
+
+
+                        
+
+                        <div className='d-flex gap-3 align-items-center'>
+                            <div>
+                                <h6 className='md-font'>Laptops</h6>
+                                <p className='p-font'>10 Items</p>
+                            </div>
+                            <img src="images/laptop.jpg" alt="television" />
+                        </div>
+
+                        <div className='d-flex gap-3 align-items-center'>
+                            <div>
+                                <h6 className='md-font'>Tablets</h6>
+                                <p className='p-font'>10 Items</p>
+                            </div>
+                            <img src="images/camera.jpg" alt="camera" />
+                        </div>
+
+                        <div className='d-flex gap-3 align-items-center'>
+                            <div>
+                                <h6 className='md-font'>Speakers</h6>
+                                <p className='p-font'>10 Items</p>
+                            </div>
+                            <img src="images/speaker.jpg" alt="wrist watches" />
+                        </div>
+
+                        <div className='d-flex gap-3 align-items-center'>
+                            <div>
+                                <h6 className='md-font'>Appliances</h6>
+                                <p className='p-font'>10 Items</p>
+                            </div>
+                            <img src="images/homeapp.jpg" alt="music and games" />
+                        </div> 
+                        
                     </div>
                 </div>
             </div>
-        </section>
+
+        </Container>
     )
 }
 
