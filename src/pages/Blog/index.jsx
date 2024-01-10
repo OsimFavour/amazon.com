5f33b7b7f08438417f29020d18bdb74401f940fe
OsimFavour <famos204@gmail.com>
2024-01-10 12:25:25 +0100

    Refactor to add Container Component

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -3,6 +3,7 @@ import Meta from '../../components/Meta'
 import BreadCrumb from '../../components/BreadCrumb'
 import BlogCard from '../../components/BlogCard'
 import BlogCategories from './BlogCategories'
+import Container from '../../components/Container'
 
 
 const Blog = () => {
@@ -11,41 +12,39 @@ const Blog = () => {
             <Meta title='Me Blog'/>
             <BreadCrumb title='Blogs'/>
 
-            <div className="blog-wrapper home-wrapper-2 py-5">
-                <div className="container">
-                    <div className="row">
+            <Container class1="blog-wrapper home-wrapper-2 py-5">
+                <div className="row">
 
-                        <div className="col-3">
+                    <div className="col-3">
+                        
+                        <BlogCategories/>
+
+                    </div>
+
+
+                    <div className="col-9">
+                        <div className="row">
                             
-                            <BlogCategories/>
-
-                        </div>
-
-
-                        <div className="col-9">
-                            <div className="row">
-                                
-                                <div className="col-6 mb-4">
-                                    <BlogCard/>
-                                </div>
-
-                                <div className="col-6 mb-3">
-                                    <BlogCard/>
-                                </div>
-
-                                <div className="col-6 mb-3">
-                                    <BlogCard/>
-                                </div>
-
-                                <div className="col-6 mb-3">
-                                    <BlogCard/>
-                                </div>
-
+                            <div className="col-6 mb-4">
+                                <BlogCard/>
                             </div>
+
+                            <div className="col-6 mb-3">
+                                <BlogCard/>
+                            </div>
+
+                            <div className="col-6 mb-3">
+                                <BlogCard/>
+                            </div>
+
+                            <div className="col-6 mb-3">
+                                <BlogCard/>
+                            </div>
+
                         </div>
                     </div>
                 </div>
-            </div>
+            </Container>
         </>
     )
 }
