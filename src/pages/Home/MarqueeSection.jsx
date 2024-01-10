35d1f86e3919fbf6bae59a6fb5634ea8f0abfbb4
OsimFavour <famos204@gmail.com>
2024-01-10 12:19:32 +0100

    Refactor to add Container Component

--- src/pages/Home/MarqueeSection.jsx
+++ src/pages/Home/MarqueeSection.jsx
@@ -1,53 +1,52 @@
 import React from 'react'
 import Marquee from "react-fast-marquee"
+import Container from '../../components/Container'
 
 const MarqueeSection = () => {
     return (
-        <section className="marquee-wrapper py-5">
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <div className="marquee-inner-wrapper card-wrapper">
-                            <Marquee className='d-flex'>
+        <Container class1="marquee-wrapper py-5">
+            <div className="row">
+                <div className="col-12">
+                    <div className="marquee-inner-wrapper card-wrapper">
+                        <Marquee className='d-flex'>
 
-                                <div className='mx-4 w-25'>
-                                    <img src="images/brand-01.png" alt="brand" />
-                                </div>
+                            <div className='mx-4 w-25'>
+                                <img src="images/brand-01.png" alt="brand" />
+                            </div>
 
-                                <div className='mx-4 w-25'>
-                                    <img src="images/brand-02.png" alt="brand" />
-                                </div>
+                            <div className='mx-4 w-25'>
+                                <img src="images/brand-02.png" alt="brand" />
+                            </div>
 
-                                <div className='mx-4 w-25'>
-                                    <img src="images/brand-03.png" alt="brand" />
-                                </div>
+                            <div className='mx-4 w-25'>
+                                <img src="images/brand-03.png" alt="brand" />
+                            </div>
 
-                                <div className='mx-4 w-25'>
-                                    <img src="images/brand-04.png" alt="brand" />
-                                </div>
+                            <div className='mx-4 w-25'>
+                                <img src="images/brand-04.png" alt="brand" />
+                            </div>
 
-                                <div className='mx-4 w-25'>
-                                    <img src="images/brand-05.png" alt="brand" />
-                                </div>
+                            <div className='mx-4 w-25'>
+                                <img src="images/brand-05.png" alt="brand" />
+                            </div>
 
-                                <div className='mx-4 w-25'>
-                                    <img src="images/brand-06.png" alt="brand" />
-                                </div>
+                            <div className='mx-4 w-25'>
+                                <img src="images/brand-06.png" alt="brand" />
+                            </div>
 
-                                <div className='mx-4 w-25'>
-                                    <img src="images/brand-07.png" alt="brand" />
-                                </div>
+                            <div className='mx-4 w-25'>
+                                <img src="images/brand-07.png" alt="brand" />
+                            </div>
 
-                                <div className='mx-4 w-25'>
-                                    <img src="images/brand-08.png" alt="brand" />
-                                </div>
+                            <div className='mx-4 w-25'>
+                                <img src="images/brand-08.png" alt="brand" />
+                            </div>
 
-                            </Marquee>
-                        </div>
+                        </Marquee>
                     </div>
                 </div>
             </div>
-        </section>
+        </Container>
     )
 }
 
