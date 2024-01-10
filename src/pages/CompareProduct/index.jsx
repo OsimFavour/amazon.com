198fba43c5e446a5daf3536e8a1fbf8d5353dbe5
OsimFavour <famos204@gmail.com>
2024-01-10 12:23:49 +0100

    Refactor to add Container Component

--- src/pages/CompareProduct/index.jsx
+++ src/pages/CompareProduct/index.jsx
@@ -3,6 +3,7 @@ import Meta from '../../components/Meta'
 import BreadCrumb from '../../components/BreadCrumb'
 import Color from '../../components/Color'
 import CompareProductCard from './CompareProductCard'
+import Container from '../../components/Container'
 
 
 const CompareProduct = () => {
@@ -11,8 +12,7 @@ const CompareProduct = () => {
       <Meta title='Compare Products'/>
       <BreadCrumb title='Compare Products'/>
 
-      <div className="compare-product-wrapper py-5 home-wrapper-2">
-        <div className="container">
+      <Container class1="compare-product-wrapper py-5 home-wrapper-2">
           <div className="row">
 
             <CompareProductCard/>
@@ -21,8 +21,7 @@ const CompareProduct = () => {
 
             
           </div>
-        </div>
-      </div>
+      </Container>
 
     </>
   )
