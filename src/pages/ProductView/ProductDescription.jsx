c770e59ef778e8c550909d048ad1284921bcafa5
OsimFavour <famos204@gmail.com>
2024-01-10 12:14:50 +0100

    Refactor to add Container Component

--- src/pages/ProductView/ProductDescription.jsx
+++ src/pages/ProductView/ProductDescription.jsx
@@ -1,38 +1,39 @@
 import React from 'react'
+import Container from '../../components/Container'
 
 const ProductDescription = () => {
   return (
-    <div className='description-wrapper py-5 home-wrapper-2'>
-        <div className='container'>
-            <div className='row'>
-                <div className='col-12'>
-                    
-                    <div>
-                        <h4>Description</h4>
-                        <p className='shadow'>
-                            'At vero eos et accusamus et iusto odio dignissimos 
-                            ducimus qui blanditiis praesentium voluptatum deleniti 
-                            atque corrupti quos dolores et quas molestias excepturi
-                            sint occaecati cupiditate non provident, similique sunt
-                            in culpa qui officia deserunt mollitia animi, id est 
-                            laborum et dolorum fuga. Et harum quidem rerum facilis 
-                            est et expedita distinctio. Nam libero tempore, cum 
-                            soluta nobis est eligendi optio cumque nihil impedit 
-                            quo minus id quod maxime placeat facere possimus, omnis
-                            voluptas assumenda est, omnis dolor repellendus. 
-                            Temporibus autem quibusdam et aut officiis debitis 
-                            aut rerum necessitatibus saepe eveniet ut et voluptates 
-                            repudiandae sint et molestiae non recusandae. Itaque 
-                            earum rerum hic tenetur a sapiente delectus, ut aut 
-                            reiciendis voluptatibus maiores alias consequatur aut 
-                            perferendis doloribus asperiores repellat.'
-                        </p>
-                    </div>
+    <Container class1='description-wrapper py-5 home-wrapper-2'>
 
+        <div className='row'>
+            <div className='col-12'>
+                
+                <div>
+                    <h4>Description</h4>
+                    <p className='shadow'>
+                        'At vero eos et accusamus et iusto odio dignissimos 
+                        ducimus qui blanditiis praesentium voluptatum deleniti 
+                        atque corrupti quos dolores et quas molestias excepturi
+                        sint occaecati cupiditate non provident, similique sunt
+                        in culpa qui officia deserunt mollitia animi, id est 
+                        laborum et dolorum fuga. Et harum quidem rerum facilis 
+                        est et expedita distinctio. Nam libero tempore, cum 
+                        soluta nobis est eligendi optio cumque nihil impedit 
+                        quo minus id quod maxime placeat facere possimus, omnis
+                        voluptas assumenda est, omnis dolor repellendus. 
+                        Temporibus autem quibusdam et aut officiis debitis 
+                        aut rerum necessitatibus saepe eveniet ut et voluptates 
+                        repudiandae sint et molestiae non recusandae. Itaque 
+                        earum rerum hic tenetur a sapiente delectus, ut aut 
+                        reiciendis voluptatibus maiores alias consequatur aut 
+                        perferendis doloribus asperiores repellat.'
+                    </p>
                 </div>
+
             </div>
         </div>
-    </div>
+        
+    </Container>
   )
 }
 
