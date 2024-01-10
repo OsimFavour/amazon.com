c7809130b9bbd5d1fc990e0c3db527e22cba7606
OsimFavour <famos204@gmail.com>
2024-01-10 12:24:52 +0100

    Refactor to add Container Component

--- src/pages/BlogView/index.jsx
+++ src/pages/BlogView/index.jsx
@@ -5,6 +5,7 @@ import BlogCategories from '../Blog/BlogCategories'
 import { Link } from 'react-router-dom'
 import { FaLongArrowAltLeft } from 'react-icons/fa'
 import blog from '../../assets/images/blog.jpg'
+import Container from '../../components/Container'
 
 
 const BlogView = () => {
@@ -13,52 +14,51 @@ const BlogView = () => {
             <Meta title='Dynamic Blog Name'/>
             <BreadCrumb title='Dynamic Blog Name'/>
 
-            <div className='blog-wrapper home-wrapper-2 py-5'>
-                <div className='container'>
-                    <div className='row'>
+            <Container class1='blog-wrapper home-wrapper-2 py-5'>
+    
+                <div className='row'>
 
-                        <div className='col-3'>
-                            
-                            <BlogCategories/>
+                    <div className='col-3'>
+                        
+                        <BlogCategories/>
 
-                        </div>
+                    </div>
 
 
-                        <div className='col-9'>
+                    <div className='col-9'>
 
-                            <div className='single-blog-card'>
+                        <div className='single-blog-card'>
 
-                                <Link className='d-flex align-items-center gap-10' to='/blog'>
-                                    <FaLongArrowAltLeft className='fs-4'/>
-                                    Back to blog
-                                </Link>
+                            <Link className='d-flex align-items-center gap-10' to='/blog'>
+                                <FaLongArrowAltLeft className='fs-4'/>
+                                Back to blog
+                            </Link>
 
-                                <h3 className='title'>
-                                    A Beautiful Sunday Morning Renaissance
-                                </h3>
+                            <h3 className='title'>
+                                A Beautiful Sunday Morning Renaissance
+                            </h3>
 
-                                <img src={blog} className='img-fluid w-100 my-4' alt='blog' />
+                            <img src={blog} className='img-fluid w-100 my-4' alt='blog' />
 
-                                <p>
-                                    You're only as good as your last collection, 
-                                    which is an enormous pressure. I think there 
-                                    is something about luxury - it's not something 
-                                    people need, but it's what they want. It really 
-                                    pulls at their heart. I have a fantastic 
-                                    relationship with money.Scelerisque sociosqu 
-                                    ullamcorper urna nisl mollis vestibulum pretium 
-                                    commodo inceptos cum condimentum placerat diam 
-                                    venenatis blandit hac eget dis lacus a parturient
-                                    a accumsan nisl ante vestibulum.
-                                </p>
-
-                            </div>
+                            <p>
+                                You're only as good as your last collection, 
+                                which is an enormous pressure. I think there 
+                                is something about luxury - it's not something 
+                                people need, but it's what they want. It really 
+                                pulls at their heart. I have a fantastic 
+                                relationship with money.Scelerisque sociosqu 
+                                ullamcorper urna nisl mollis vestibulum pretium 
+                                commodo inceptos cum condimentum placerat diam 
+                                venenatis blandit hac eget dis lacus a parturient
+                                a accumsan nisl ante vestibulum.
+                            </p>
 
                         </div>
+
                     </div>
                 </div>
-
-            </div>
+            
+            </Container>
 
         </>
     )
