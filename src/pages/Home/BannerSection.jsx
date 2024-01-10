9a9afb6d6c5462465dd7dcfb8f3beba711f71eec
OsimFavour <famos204@gmail.com>
2024-01-10 12:22:17 +0100

    Refactor to add Container Component

--- src/pages/Home/BannerSection.jsx
+++ src/pages/Home/BannerSection.jsx
@@ -1,38 +1,38 @@
 import React from 'react'
 import SmallBannerSection from './SmallBannerSection'
 import { Link } from 'react-router-dom'
+import Container from '../../components/Container'
 
 const BannerSection = () => {
     return (
-        <section className="home-wrapper-1 pt-5 py-5">
-            <div className="container">
-                <div className="row">
 
-                    <div className="col-6">
-                        
-                            <div className="main-banner position-relative">
-                                <img 
-                                    src="images/new-banner.jpg" 
-                                    className='img-fluid rounded-3'
-                                    alt="main banner" 
-                                />
-                                <div className="main-banner-content position-absolute">
-                                    <h4>Supercharged For Pros</h4>
-                                    <h5>iPad S13+ Pro.</h5>
-                                    <p>From $999.00 or $41.62/mo</p>
-                                    <Link className='button'>BUY NOW</Link>
-                                </div>
-                            </div>
-                        
+        <Container class1='home-wrapper-1 pt-5 py-5'>
+            <div className="row">
+
+                <div className="col-6">
+                    
+                    <div className="main-banner position-relative">
+                        <img 
+                            src="images/new-banner.jpg" 
+                            className='img-fluid rounded-3'
+                            alt="main banner" 
+                        />
+                        <div className="main-banner-content position-absolute">
+                            <h4>Supercharged For Pros</h4>
+                            <h5>iPad S13+ Pro.</h5>
+                            <p>From $999.00 or $41.62/mo</p>
+                            <Link className='button'>BUY NOW</Link>
+                        </div>
                     </div>
-
-                    <div className="col-6">
-                        <SmallBannerSection/>
-                    </div>
-
+                    
                 </div>
+
+                <div className="col-6">
+                    <SmallBannerSection/>
+                </div>
+
             </div>
-        </section> 
+        </Container>
     )
 }
 
