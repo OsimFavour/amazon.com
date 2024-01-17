4560ace5dd76eac9a7a72ea7940d65831eabbe22
OsimFavour <famos204@gmail.com>
2024-01-17 15:59:18 +0100

    Add Toast for notifications

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from '../Header'
 import { Outlet } from 'react-router-dom'
 import Footer from '../Footer'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 
 const Layout = () => {
@@ -10,6 +12,20 @@ const Layout = () => {
             <Header/>
                 <Outlet/>
             <Footer/>
+            
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            
         </>
     )
 }
