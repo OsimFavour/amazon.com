b3ef846805a4f8d8e3bbec5eb37770c177122e64
OsimFavour <famos204@gmail.com>
2024-01-17 15:57:36 +0100

    Add Signup validation and state management tool

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -3,9 +3,40 @@ import Meta from '../../components/Meta'
 import BreadCrumb from '../../components/BreadCrumb'
 import Container from '../../components/Container'
 import CustomInput from '../../components/CustomInput'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+import { useDispatch } from 'react-redux'
+import { registerUser } from '../../features/user/userSlice'
+
+
+const signUpSchema = yup.object({
+    first_name: yup.string().required('First Name is Required'),
+    last_name: yup.string().required('Last Name is Required'),
+    password: yup.string().required('Password is Required'),
+    email: yup.string().required().email('Email Should Be Valid'),
+    phone_no: yup.string().required('Phone Number is Required'),
+  });
+
 
 
 const SignUp = () => {
+
+    const dispatch = useDispatch()
+    const formik = useFormik({
+        initialValues: {                             
+            first_name: '',
+            last_name: '',
+            password: '',
+            email: '',
+            phone_no: '',
+        },
+        validationSchema: signUpSchema,
+        onSubmit: values => {
+          dispatch(registerUser(values))
+        },
+    });
+
+
     return (
         <>
             <Meta title='Sign Up'/>
@@ -19,12 +50,84 @@ const SignUp = () => {
 
                         <div className='auth-card'>
                             <h3>Sign Up</h3>
-                            <form action='' className='auth-form'>
+                            <form 
+                                action='' 
+                                className='auth-form'
+                                onSubmit={formik.handleSubmit}
+                            >
                                 
-                                <CustomInput type='text' name='firstName' placeholder='First Name' />
-                                <CustomInput type='text' name='lastName' placeholder='Last Name' />
-                                <CustomInput type='email' name='email' placeholder='Email' />
-                                <CustomInput type='password' name='password' placeholder='Password' className='mt-1 mb-3' />
+                                <CustomInput 
+                                    type='text' 
+                                    name='first_name' 
+                                    placeholder='First Name' 
+                                    value={formik.values.first_name}
+                                    onChange={formik.handleChange('first_name')}
+                                    onBlur={formik.handleBlur('first_name')}
+                                />
+
+                                <div className="error">
+                                    {formik.touched.first_name && formik.errors.first_name}
+                                </div>
+
+                                
+
+
+                                <CustomInput 
+                                    type='text' 
+                                    name='last_name' 
+                                    placeholder='Last Name' 
+                                    value={formik.values.last_name}
+                                    onChange={formik.handleChange('last_name')}
+                                    onBlur={formik.handleBlur('last_name')}
+                                />
+
+                                <div className="error">
+                                    {formik.touched.last_name && formik.errors.last_name}
+                                </div>
+
+
+                                <CustomInput 
+                                    type='password' 
+                                    name='password' 
+                                    placeholder='Password'
+                                    value={formik.values.password}
+                                    onChange={formik.handleChange('password')}
+                                    onBlur={formik.handleBlur('password')}
+                                />
+
+                                <div className="error">
+                                    {formik.touched.password && formik.errors.password}
+                                </div>
+                                
+                                
+                                <CustomInput 
+                                    type='email' 
+                                    name='email' 
+                                    placeholder='Email' 
+                                    value={formik.values.email}
+                                    onChange={formik.handleChange('email')}
+                                    onBlur={formik.handleBlur('email')}
+                                />
+
+                                <div className="error">
+                                    {formik.touched.email && formik.errors.email}
+                                </div>
+
+
+                                <CustomInput 
+                                    type='tel' 
+                                    name='phone_no' 
+                                    placeholder='Phone Number'  
+                                    className='mt-1 mb-3' 
+                                    value={formik.values.phone_no}
+                                    onChange={formik.handleChange('phone_no')}
+                                    onBlur={formik.handleBlur('phone_no')}
+                                />
+
+                                <div className="error">
+                                    {formik.touched.phone_no && formik.errors.phone_no}
+                                </div>
+
 
                                 <div>
 
