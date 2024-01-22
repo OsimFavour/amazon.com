ccc6b0f0948f1e98bf8aadb0d845f19e225d7b68
OsimFavour <famos204@gmail.com>
2024-01-22 16:05:51 +0100

    Create Register and login reducer

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -24,6 +24,24 @@ export const registerUser = createAsyncThunk(
     }
 )
 
+
+
+export const loginUser = createAsyncThunk(
+    'auth/login',
+    async (userData, thunkAPI) => {
+        try {
+            console.log("sdfdf", userData)
+            return await authService.login(userData)
+        } 
+        catch (error) {
+            console.log("errrrrrr ", error)
+            return thunkAPI.rejectWithValue(error)
+        }
+    }
+)
+
+
+
 export const authSlice = createSlice({
     name: 'auth',
     initialState,
@@ -51,8 +69,37 @@ export const authSlice = createSlice({
                     toast.error(action.error)
                 }
             })
+
+
+            .addCase(loginUser.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(loginUser.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isError = false
+                state.isSuccess = true
+                state.user = action.payload
+                console.log(action.payload)
+                if (state.isSuccess === true) {
+                    localStorage.setItem('token', action.payload.token)
+                    toast.info('User Logged Successfully')
+                }
+            })
+            .addCase(loginUser.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.isSuccess = false
+                state.message = action.error
+                if (state.isError === true) {
+                    toast.error(action.error)
+                }
+            })
+
+
     }
 })
 
+
+// console.log(authSlice);
 
 export default authSlice.reducer
